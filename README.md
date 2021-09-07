6250aaf9ae3acb5e52fb91368ae3a52d66b61d5b
Pablo Castillo <psc88sdrd@gmail.com>
2021-09-07 00:38:38 -0300

    Importado de appbar, makeStyles, Toolbar y agregado de logo en el Header por displeyDesktop

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ##Pasos para completar el proyecto de reserva hotelera
 
 - Limpieza de la plantilla
-- App.js - Componentes header, home, serachpages
+- App.js : Componentes header, home, serachpages
 - Instalacion de libreria add react-router-dom (para dividir en la web los componentes)
 - Enrutar Home y SerachPage con react-router-dom
 - importamos Router, Switch, Route; y creamos los switch para poner las rutas
-- 
+- Header :  displayDesktop() , displayMobile() 
+- instalamos add @material-ui/core and @material-ui/icons
+- Header : displayDesktop (agregar imagen y navbar)
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import { AppBar, makeStyles, Toolbar } from "@material-ui/core"
+import { CallMissedSharp } from "@material-ui/icons"
+import { useState } from "react"
+import logo from "../imagenes/logoHotel.png"
 
 const Header = () => {
+    const [mobile, setMobile] = useState(false)
+    const classes = useStyle()
+    const displayMobile = () => {}
+    const displayDesktop = () => (
+            <Toolbar className={ classes.toolbar }>
+                <img src={logo} className={ classes.logo } />
+            </Toolbar>
+    )
+    
     return(
-        <div>
-            <h1>Encabezamiento</h1>
-        </div>
+        <AppBar>
+            {
+            mobile ? displayMobile() : displayDesktop()
+            }
+        </AppBar>
     )
 }
+
+const useStyle = makeStyles((theme) => ({
+    toolbar: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    logo: {
+        height: "180px",
+        margin: theme.spacing(1,0,1,0),
+        objectFit: "contain",
+    }
+}))
 
 export default Header
